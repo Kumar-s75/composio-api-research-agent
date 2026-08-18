5a5339909dfce4f72d5fd79583fe7681e79395da
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-08-18 18:58:38 +0530

    added scoring

---
+++ src/scoring/index.ts
@@ -0,0 +1,8 @@
+export {
+  deterministicBuildabilityAssessmentSchema,
+  deterministicBuildabilityBlockerSchema,
+  deterministicBuildabilityReasonSchema,
+  deterministicBuildabilityVerdictSchema,
+  scoreBuildability,
+} from "./buildability.js";
+export type { DeterministicBuildabilityAssessment } from "./buildability.js";
