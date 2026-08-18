ef4113683c08d762f1f8f6fff9398116b4396c5b
Kumar Saurabh <kumarsaurabh27d@gmail.com>
2026-08-18 18:57:30 +0530

    implemnting composio runner

---
+++ src/runner/index.ts
@@ -0,0 +1,10 @@
+export {
+  appIdFromName,
+  BatchResearchRunner,
+  toResearchApp,
+} from "./batch-research-runner.js";
+export type {
+  BatchResearchRunnerOptions,
+  BatchResearchSummary,
+  RunnableResearchAgent,
+} from "./batch-research-runner.js";
